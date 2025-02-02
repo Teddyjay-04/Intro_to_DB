2a29f84e4dae2802da6cced4a1dfebb17d45f434
James Teddy <136697800+Teddyjay-04@users.noreply.github.com>
2025-02-02 21:36:20 +0000

    Create task_5.sql

---
+++ task_5.sql
@@ -0,0 +1,13 @@
+-- Display the full description of the 'books' table from the specified database
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = DATABASE() 
+    AND TABLE_NAME = 'books';
