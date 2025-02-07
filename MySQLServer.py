2ffa229e5396fefb941f7d57eba147b782be8b08
James Teddy <136697800+Teddyjay-04@users.noreply.github.com>
2025-02-07 21:15:21 +0000

    Done

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,30 +1,37 @@
 import mysql.connector
-from mysql.connector import Error
+from mysql.connector import errorcode
 
-def create_database():
+def create_database(cursor):
     try:
-        connection = mysql.connector.connect(
-            host = 'localhost',
-            user = 'root',
-            password = 'Abi_gail@25'
-        ) 
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Database 'alx_book_store' created successfully!")
+    except mysql.connector.Error as err:
+        print(f"Failed creating database: {err}")
 
-        if connection.is_connected():
-            cursor = connection.cursor()
+def main():
+    try:
+        # Connect to MySQL server
+        conn = mysql.connector.connect(
+            host='Popoi',
+            user='root',  # replace with your MySQL username
+            password='Asphalt1234500#@'  # replace with your MySQL password
+        )
+        cursor = conn.cursor()
 
-            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        # Create the database
+        create_database(cursor)
 
-            print("Database 'alx_book_store' created successfully")
+        # Close the cursor and connection
+        cursor.close()
+        conn.close()
 
-    except Error as e:
-        print(f"Error: {e}")
-
-    finally:
-        if connection.is_connected():
-            cursor.close()
-            connection.close()
-            print("MySQL connection is closed.")    
-
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
 
 if __name__ == "__main__":
-    create_database()   
+    main()
