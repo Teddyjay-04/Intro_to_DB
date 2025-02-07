91850361bda5b8d7db3fc83ba90ff0af361609c7
James Teddy <136697800+Teddyjay-04@users.noreply.github.com>
2025-02-07 21:07:40 +0000

    Done

--- task_6.sql
+++ task_6.sql
@@ -1,11 +1,6 @@
--- Use the correct database
 USE alx_book_store;
 
--- Check if customer with customer_id = 2 exists
-SELECT * FROM customers WHERE customer_id = 2;
-
--- Check if customer with customer_id = 3 exists
-SELECT * FROM customers WHERE customer_id = 3;
-
--- Check if customer with customer_id = 4 exists
-SELECT * FROM customers WHERE customer_id = 4;
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness  Ave."),
+        (3, "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness  Ave."),
+        (4, "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness  Ave.");
