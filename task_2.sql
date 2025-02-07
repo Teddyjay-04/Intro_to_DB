6b636646d726661da72fe5d25a4c174e28e064d1
James Teddy <136697800+Teddyjay-04@users.noreply.github.com>
2025-02-07 21:01:09 +0000

    Done

--- task_2.sql
+++ task_2.sql
@@ -1,47 +1,36 @@
--- Ensure we are using the correct database
-USE alx_book_store;
+CREATE TABLE Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id),
+)
+#HHHHDHDD
+CREATE TABLE Authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215)
+)
 
--- Check if 'authors' table exists
-SELECT 
-    TABLE_NAME 
-FROM 
-    INFORMATION_SCHEMA.TABLES 
-WHERE 
-    TABLE_SCHEMA = DATABASE() 
-    AND TABLE_NAME = 'authors';
+CREATE TABLE Customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    customer_email VARCHAR(215),
+    address TEXT
+)
 
--- Check if 'books' table exists
-SELECT 
-    TABLE_NAME 
-FROM 
-    INFORMATION_SCHEMA.TABLES 
-WHERE 
-    TABLE_SCHEMA = DATABASE() 
-    AND TABLE_NAME = 'books';
+CREATE TABLE Orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+)
 
--- Check if 'customers' table exists
-SELECT 
-    TABLE_NAME 
-FROM 
-    INFORMATION_SCHEMA.TABLES 
-WHERE 
-    TABLE_SCHEMA = DATABASE() 
-    AND TABLE_NAME = 'customers';
-
--- Check if 'orders' table exists
-SELECT 
-    TABLE_NAME 
-FROM 
-    INFORMATION_SCHEMA.TABLES 
-WHERE 
-    TABLE_SCHEMA = DATABASE() 
-    AND TABLE_NAME = 'orders';
-
--- Check if 'order_details' table exists
-SELECT 
-    TABLE_NAME 
-FROM 
-    INFORMATION_SCHEMA.TABLES 
-WHERE 
-    TABLE_SCHEMA = DATABASE() 
-    AND TABLE_NAME = 'order_details';
+CREATE TABLE  IF NOT EXIST Order_Details (
+    order_details_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+)
